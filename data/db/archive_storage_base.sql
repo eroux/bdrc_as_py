ee67416342add7462d626a07a233b6ef764a1ed7
eroux <elie.roux@telecom-bretagne.eu>
2023-07-28 19:12:44 +0200

    update schema

--- data/db/archive_storage_base.sql
+++ data/db/archive_storage_base.sql
@@ -9,7 +9,7 @@ CREATE TABLE `storage`.`objects` (
   `bdrc_id` varchar(32) CHARACTER SET ascii NOT NULL COMMENT 'the BDRC RID (ex: W22084), unique persistent identifier, ASCII string no longer than 32 characters',
   `created_at` timestamp COMMENT 'the timestamp of the creation of the object, or the equivalent object in a previous archive storage',
   `last_modified_at` timestamp COMMENT 'the timestamp of the last known modification',
-  `root_id` tinyint UNSIGNED NOT NULL COMMENT 'the OCFL stoage root id that the object is stored in'
+  `root` tinyint UNSIGNED NOT NULL COMMENT 'the OCFL stoage root id that the object is stored in'
 );
 
 CREATE TABLE `storage`.`roots` (
@@ -28,31 +28,58 @@ CREATE TABLE `storage`.`files` (
 );
 
 CREATE TABLE `storage`.`paths` (
-  `file_id` int,
+  `file` int UNSIGNED,
   `storage_object` mediumint UNSIGNED,
   `path` varchar(1024) CHARACTER SET utf8mb4 COMMENT 'Unicode string (256 Unicode characters max) representing the (case sensitive) content paths in OCFL objects for each content file.'
 );
 
+CREATE TABLE `storage`.`users` (
+  `id` smallint UNSIGNED PRIMARY KEY,
+  `name` varchar(256) CHARACTER SET utf8mb4 COMMENT 'A name for the user, limitted to 64 characters'
+);
+
 CREATE TABLE `etexts`.`file_info` (
-  `content_file_id` int UNSIGNED PRIMARY KEY,
-  `image_type` ENUM ('jpg', 'png', 'single_image_tiff', 'jp2') NOT NULL
+  `content_file` int UNSIGNED PRIMARY KEY,
+  `etext_type` ENUM ('doc', 'docx', 'tibet_doc', 'rtf', 'pdf', 'xml', 'tei_xml', 'txt', 'indd') NOT NULL,
+  `nb_unicode_chars` mediumint UNSIGNED COMMENT 'the number of Unicode characters'
+);
+
+CREATE TABLE `etexts`.`ocfl_versions` (
+  `storage_object` mediumint UNSIGNED PRIMARY KEY,
+  `version` smallint UNSIGNED COMMENT 'The OCFL version of the object',
+  `created_at` timestamp NOT NULL COMMENT 'the creation time',
+  `user` smallint UNSIGNED NOT NULL COMMENT 'the user who created the version',
+  `message` varchar(256) CHARACTER SET utf8mb4 COMMENT 'the message of the version in Unicode, truncated to 64 characters'
+);
+
+CREATE TABLE `etexts`.`ocfl_objects_latest_version` (
+  `storage_object` mediumint UNSIGNED PRIMARY KEY,
+  `latest_version` smallint UNSIGNED NOT NULL COMMENT 'The latest OCFL version as integer'
+);
+
+CREATE TABLE `etexts`.`ocfl_logical_paths` (
+  `storage_file` int UNSIGNED,
+  `storage_object` mediumint UNSIGNED,
+  `ocfl_object_version` smallint UNSIGNED NOT NULL COMMENT 'the version of the object that corresponds to this logical path'
 );
 
 CREATE TABLE `images`.`file_infos` (
-  `content_file_id` int UNSIGNED PRIMARY KEY,
+  `storage_file` int UNSIGNED PRIMARY KEY,
   `image_type` ENUM ('jpg', 'png', 'single_image_tiff', 'jp2') NOT NULL,
   `image_mode` ENUM ('1', 'L', 'RGB', 'RGBA', 'CMYK', 'P', 'OTHER') NOT NULL,
   `tiff_compression` ENUM ('raw', 'tiff_ccitt', 'group3', 'group4', 'tiff_lzw', 'tiff_jpeg', 'jpeg', 'tiff_adobe_deflate', 'lzma', 'other') COMMENT 'names are from PIL version 10',
   `width` smallint UNSIGNED NOT NULL COMMENT 'width of the bitmap (not taking a potential exif rotation into account)',
   `height` smallint UNSIGNED NOT NULL COMMENT 'height of the bitmap (not taking a potential exif rotation into account)',
-  `quality` tinyint COMMENT 'relevant only for jpg, png and single_image_tiff encoded as jpg: quality of encoding. JPEG is represented between 0 and 100. For PNG this column encodes the compression between 0 and 9.',
+  `quality` tinyint UNSIGNED COMMENT 'relevant only for jpg, png and single_image_tiff encoded as jpg: quality of encoding. JPEG is represented between 0 and 100. For PNG this column encodes the compression between 0 and 9.',
   `bps` tinyint UNSIGNED NOT NULL COMMENT 'bits per sample',
   `recorded_date` timestamp COMMENT 'the timestamp recorded in the metadata'
 );
 
-CREATE UNIQUE INDEX ``storage`.objects_index_0` ON `storage`.`objects` (`bdrc_id`, `root_id`);
+CREATE UNIQUE INDEX ``storage`.objects_index_0` ON `storage`.`objects` (`bdrc_id`, `root`);
 
 CREATE UNIQUE INDEX ``storage`.files_index_1` ON `storage`.`files` (`sha256`, `size`);
+
+CREATE UNIQUE INDEX ``etexts`.ocfl_versions_index_0` ON `etexts`.`ocfl_versions` (`storage_object`, `version`);
 
 ALTER TABLE `storage`.`objects` COMMENT = 'Objects kept on archive storage';
 
@@ -62,14 +89,32 @@ ALTER TABLE `storage`.`files` COMMENT = 'Table of all the (deduplicated) actual 
 
 ALTER TABLE `storage`.`paths` COMMENT = 'Table connecting files and their content paths in storage objects';
 
+ALTER TABLE `storage`.`users` COMMENT = 'While not used in the other storage tables, users should be shared';
+
+ALTER TABLE `etexts`.`file_info` COMMENT = 'Domain-specific information record about relevant files (not every file needs to have an entry in this table)';
+
+ALTER TABLE `etexts`.`ocfl_versions` COMMENT = 'A table recording the OCFL versions of all the objects';
+
+ALTER TABLE `etexts`.`ocfl_objects_latest_version` COMMENT = 'A table recording the most recent version of an OCFL object, as an integer';
+
+ALTER TABLE `etexts`.`ocfl_logical_paths` COMMENT = 'This table represents the logical state of each version (see OCFL spec)';
+
 ALTER TABLE `images`.`file_infos` COMMENT = 'Table containing information about image files';
 
-ALTER TABLE `storage`.`objects` ADD FOREIGN KEY (`root_id`) REFERENCES `storage`.`roots` (`id`);
+ALTER TABLE `storage`.`objects` ADD FOREIGN KEY (`root`) REFERENCES `storage`.`roots` (`id`);
 
-ALTER TABLE `storage`.`paths` ADD FOREIGN KEY (`file_id`) REFERENCES `storage`.`files` (`id`);
+ALTER TABLE `storage`.`paths` ADD FOREIGN KEY (`file`) REFERENCES `storage`.`files` (`id`);
 
 ALTER TABLE `storage`.`paths` ADD FOREIGN KEY (`storage_object`) REFERENCES `storage`.`objects` (`id`);
 
-ALTER TABLE `etexts`.`file_info` ADD FOREIGN KEY (`content_file_id`) REFERENCES `storage`.`files` (`id`);
+ALTER TABLE `etexts`.`file_info` ADD FOREIGN KEY (`content_file`) REFERENCES `storage`.`files` (`id`);
 
-ALTER TABLE `images`.`file_infos` ADD FOREIGN KEY (`content_file_id`) REFERENCES `storage`.`files` (`id`);
+ALTER TABLE `etexts`.`ocfl_versions` ADD FOREIGN KEY (`storage_object`) REFERENCES `storage`.`objects` (`id`);
+
+ALTER TABLE `etexts`.`ocfl_objects_latest_version` ADD FOREIGN KEY (`storage_object`) REFERENCES `storage`.`objects` (`id`);
+
+ALTER TABLE `etexts`.`ocfl_logical_paths` ADD FOREIGN KEY (`storage_file`) REFERENCES `storage`.`files` (`id`);
+
+ALTER TABLE `etexts`.`ocfl_logical_paths` ADD FOREIGN KEY (`storage_object`) REFERENCES `storage`.`objects` (`id`);
+
+ALTER TABLE `images`.`file_infos` ADD FOREIGN KEY (`storage_file_id`) REFERENCES `storage`.`files` (`id`);
